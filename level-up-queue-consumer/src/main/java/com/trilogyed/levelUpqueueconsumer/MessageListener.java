ac4bfcf3fa03e1ea5e52d021af2180f80c9b1627
Margaret Rumble <megrumble@gmail.com>
2019-08-24 08:36:58 -0400

    level-up-queue-consumer

---
+++ level-up-queue-consumer/src/main/java/com/trilogyed/levelUpqueueconsumer/MessageListener.java
@@ -0,0 +1,28 @@
+package com.trilogyed.levelUpqueueconsumer;
+
+import com.trilogyed.levelUpqueueconsumer.util.feign.LevelUpServiceClient;
+import com.trilogyed.levelUpqueueconsumer.util.messages.Member;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+@Service
+public class MessageListener {
+
+    @Autowired
+    LevelUpServiceClient client;
+
+    public MessageListener(LevelUpServiceClient client) {
+        this.client = client;
+    }
+    @RabbitListener(queues = LevelUpQueueConsumerApplication.QUEUE_NAME)
+    public void receivemessages(Member msg) {
+        if( msg.getLevelUpId() ==0) {
+            client.addMember(msg);
+        } else {
+            client.updateMember(msg.getLevelUpId(), msg);
+        }
+    }
+}
---
+++ level-up-queue-consumer/src/main/java/com/trilogyed/levelUpqueueconsumer/util/feign/LevelUpServiceClient.java
@@ -0,0 +1,18 @@
+package com.trilogyed.levelUpqueueconsumer.util.feign;
+
+import com.trilogyed.levelUpqueueconsumer.util.messages.Member;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/levelUp")
+@FeignClient(name = "level-up-service")
+public interface LevelUpServiceClient {
+
+    @PostMapping
+    public Member addMember(@RequestBody Member member);
+
+    @PutMapping("/{id}")
+    public void updateMember(@PathVariable int id, @RequestBody Member member);
+
+
+}
